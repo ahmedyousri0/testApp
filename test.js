493c3f55c1ac9fd7fe4df2a09898c40cbe9d3a8f
Ayman <enkhalifapro@gmail.com>
2017-01-21 13:12:41 +0200

    finish adding input values to table

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 // ready
 
 /*
-$(function(){     }
-هذه معناها بعد ما تتحمل الصفحة كاملة من
-<html>  to   </html>
-ابدء تطبيق الاكواد التي بداخل
+ $(function(){     }
+ هذه معناها بعد ما تتحمل الصفحة كاملة من
+ <html>  to   </html>
+ ابدء تطبيق الاكواد التي بداخل
  {    }
-انا ابدأ اكتب اوامر جافا اسكربت مابيت الاقواس
-*/
+ انا ابدأ اكتب اوامر جافا اسكربت مابيت الاقواس
+ */
 // $  تعني يا جافا اسكربت
 /* # تعني
-استدعي ال
-id
-صاحب الاسم
-addBtn
-$("#addBtn")
-*/
+ استدعي ال
+ id
+ صاحب الاسم
+ addBtn
+ $("#addBtn")
+ */
 //$(".addBtn")  لو نقطة  .  معناها استدعي الكلاس صاحب الاسم الفالني  الاسم اللي انت هتكتبه
 /* لو هتستدعي تاج نيم مثل
  body , div
@@ -23,24 +23,28 @@ $("#addBtn")
  $("body")
  */
 
- /* .click(function(){        }
+/* .click(function(){        }
  معناها بعد الضغط نفذ الاوار مابين الاقواس
  {   }
  */
 
-$(function(){
-  // handling click even of addBtn
-  $("#addBtn").click(function(){
+$(function ()
+{
+	// handling click even of addBtn
+	$("#addBtn").click(function ()
+	{
 
 
-/* .find("tbody").append("<tr><td>new 1</td><td>new 2</td></tr>")
- ابحث عن الجدول
-tbody
-.append تعني
-ثم ضيف عليه
-صف وعامود ("<tr><td>new 1</td><td>new 2</td></tr>")
-*/
-  $(":input").css("background-color", "red");
-    $("#myTable").find("tbody").append("<tr><td>new 1</td><td>new 2</td></tr>")
-  })
+		/* .find("tbody").append("<tr><td>new 1</td><td>new 2</td></tr>")
+		 ابحث عن الجدول
+		 tbody
+		 .append تعني
+		 ثم ضيف عليه
+		 صف وعامود ("<tr><td>new 1</td><td>new 2</td></tr>")
+		 */
+		var name = $("#name").val();
+		var phone = $("#phone").val();
+		var myTag="<tr><td>" + name + "</td><td>" + phone + "</td></tr>";
+		$("#myTable").find("tbody").append(myTag)
+	});
 });
